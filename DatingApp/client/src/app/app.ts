a45583c3cf5e70a26247be3dda6266a87ab7f2f8
A-abbasinia <abbasiniaaa@gmail.com>
2025-10-12 21:51:30 +0330

    Add-AngularandSSL

---
+++ DatingApp/client/src/app/app.ts
@@ -0,0 +1,26 @@
+import {Component, inject, OnInit, signal} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,],
+
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit {
+
+  http= inject(HttpClient);
+  protected readonly title = signal('DatingApp');
+  users : any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response  => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log("Request has completed"),
+    })
+  }
+
+}
