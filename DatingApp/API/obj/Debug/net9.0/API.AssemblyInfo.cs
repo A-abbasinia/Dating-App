de4cb35b388209008a653292808e113121c082a3
A-abbasinia <abbasiniaaa@gmail.com>
2025-12-07 18:35:21 +0330

    Improve-API-Services

--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a546bd35eab25277c32558ed20efa7ecdd6f4715")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a35a217c3111d044a217a7ea4b4d030cbeff6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
